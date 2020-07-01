cb5cef4507814ee4251f08e176088a9d99b8be06
Leonardo Porto <leonardoporto11@hotmail.com>
2020-07-01 19:12:17 -0300

    Configurando banco

---
+++ appsettings.ts
@@ -0,0 +1,10 @@
+export = {
+    sqlPool: {
+        connectionLimit: 30,
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'conta'
+    }
+}
