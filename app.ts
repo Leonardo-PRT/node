ba6fbac337686f5c0673954337d4e136ae1d1769
Leonardo Porto <leonardoporto11@hotmail.com>
2020-07-01 18:00:15 -0300

    Setando rotas

--- app.ts
+++ app.ts
@@ -1,13 +1,40 @@
 import express = require('express')
 import path = require('path')
+import ejs = require('ejs')
+import lru = require('lru-cache')
 
 const app = express()
 
-app.get("/", (req, res) =>
- res.sendFile(path.join(__dirname, "/index.html")))
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.use(express.static(path.join(__dirname, "/public"), {
+	cacheControl: true,
+	etag: false,
+	maxAge: "30d"
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ejs.cache = lru(200);
+
+app.set("view engine", "ejs");
+app.use(require("express-ejs-layouts"));
+
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+	res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+	res.header("Expires", "-1");
+	res.header("Pragma", "no-cache");
+	next();
+});
+
+app.use('/', require('./routes/home'))
 
  const porta = (parseInt(process.env.PORT) || 3000)
 
 app.listen(porta, () => {
 	console.log("Executando servidor na porta " + porta);
 });
+
---
+++ routes/home.ts
@@ -0,0 +1,12 @@
+import express = require('express')
+import wrap = require('express-async-error-wrapper')
+import Conta = require('../models/conta')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index')
+
+})
+
+export = router
